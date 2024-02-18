8e68f45e26074d2c946c92d6c1127b93df939ad1
WagnerLeodoro <wagner.augustoleodoro@gmail.com>
2024-02-17 22:42:50 -0300

    Tags table and routes

---
+++ src/controllers/TagsController.js
@@ -0,0 +1,12 @@
+const knex = require('../database/knex')
+
+class TagsController {
+  async index(req, res) {
+    const { user_id } = req.params
+    const tags = await knex('movie_tags').where({ user_id })
+
+    return res.status(200).json(tags)
+  }
+}
+
+module.exports = TagsController
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,12 @@
 const { Router } = require('express')
 const userRouter = require('./users.routes')
+const movieRouter = require('./movie.routes')
+const tagsRouter = require('./tags.routes')
 
 const routes = Router()
 
 routes.use('/users', userRouter)
+routes.use('/movies', movieRouter)
+routes.use('/tags', tagsRouter)
 
 module.exports = routes
